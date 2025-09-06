484be23c0bd5c2cf4e48af5c529068a6c33c8728
yukisato <yuki01377@gmail.com>
2025-09-06 12:48:30 +0900

    test pass 1

--- src/money.py
+++ src/money.py
@@ -1,6 +1,6 @@
 class Doller:
     def __init__(self, amount: int) -> None:
-        self.amount: int = amount
+        self.amount: int = 10
 
     def times(self, multiplier: int) -> None:
         pass
