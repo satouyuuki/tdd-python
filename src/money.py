0fe71521a34ee2a3936a6e641b67288e615851d1
yukisato <yuki01377@gmail.com>
2025-09-06 17:38:03 +0900

    add comment and red test

--- src/money.py
+++ src/money.py
@@ -1,11 +1,11 @@
 class Doller:
     def __init__(self, amount: int) -> None:
-        self.amount = amount
+        self._amount = amount
 
     def times(self, multiplier: int) -> 'Doller':
-        return Doller(self.amount * multiplier)
+        return Doller(self._amount * multiplier)
 
     def equals(self, obj: object) -> bool:
         if not isinstance(obj, Doller):
             return False
-        return self.amount == obj.amount
+        return self._amount == obj._amount
--- src/money_test.py
+++ src/money_test.py
@@ -4,10 +4,8 @@ from money import Doller
 class MoneyTest(unittest.TestCase):
     def testMultiplication(self):
         five: Doller = Doller(5)
-        product: Doller = five.times(2)
-        self.assertEqual(10, product.amount)
-        product = five.times(3)
-        self.assertEqual(15, product.amount)
+        self.assertEqual(Doller(10), five.times(2))
+        self.assertEqual(Doller(15), five.times(3))
 
     def testEquality(self):
         self.assertTrue(Doller(5).equals(Doller(5)))
