519452e02c96119eb499a39f9bd3e3ab59b71100
yukisato <yuki01377@gmail.com>
2025-09-06 13:34:36 +0900

    コンパイルは通るようになった

--- src/money.py
+++ src/money.py
@@ -2,5 +2,6 @@ class Doller:
     def __init__(self, amount: int) -> None:
         self.amount = amount
 
-    def times(self, multiplier: int) -> None:
+    def times(self, multiplier: int) -> 'Doller':
         self.amount *= multiplier
+        return self
