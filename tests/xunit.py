0153ebac4131f3657ec44e17b1da2931acc958a9
yukisato <yuki01377@gmail.com>
2025-09-07 20:50:12 +0900

    add pass test

--- tests/xunit.py
+++ tests/xunit.py
@@ -1,8 +1,11 @@
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
+    def run(self):
+        self.testMethod()
     def testMethod(self):
         self.wasRun = 1
+
 test = WasRun("testMethod")
 print(test.wasRun)
 test.run()
