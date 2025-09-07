d7ca73a77185f86ec24cd6c1f527db65ab8cbf53
yukisato <yuki01377@gmail.com>
2025-09-07 18:34:45 +0900

    add pass test 仮実装

--- money/sum.py
+++ money/sum.py
@@ -3,10 +3,10 @@ from .money import Money
 from .bank import Bank
 
 class Sum(Expression):
-    def __init__(self, augend, addend) -> None:
+    def __init__(self, augend: Money, addend: Money) -> None:
         self.augend: Money = augend
         self.addend: Money = addend
 
     def reduce(self, bank: Bank, to: str) -> Money:
-        amount: int = self.augend._amount + self.addend._amount
+        amount: int = self.augend.reduce(bank, to)._amount + self.addend.reduce(bank, to)._amount
         return Money(amount, to)
