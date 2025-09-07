5fcebc2c49a465da3cbca1285a012fcbab8483ac
yukisato <yuki01377@gmail.com>
2025-09-07 13:37:17 +0900

    add pass test 仮実装

--- money/bank.py
+++ money/bank.py
@@ -5,5 +5,7 @@ from typing import cast
 
 class Bank:
     def reduce(self, source: Expression, to: str) -> Money:
+        if isinstance(source, Money):
+            return cast(Money, source)
         sum: Sum = cast(Sum, source)
         return sum.reduce(to)
