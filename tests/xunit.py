c9585146ffebfe135279c6120bd64c7a0bb05c59
yukisato <yuki01377@gmail.com>
2025-09-08 08:06:18 +0900

    add refactoring tearDownをテストする前にフラグ管理をやめる

--- tests/xunit.py
+++ tests/xunit.py
@@ -10,22 +10,14 @@ class TestCase:
 
 class WasRun(TestCase):
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
     def testMethod(self):
-        self.wasRun = 1
+        self.log += "testMethod "
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod " == test.log)
 
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-    
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
-
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
